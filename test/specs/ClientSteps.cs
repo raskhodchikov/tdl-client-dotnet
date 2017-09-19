ba4436961d84d5e3be66860143142a2f7bb22942
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-09-19 03:13:46 +0300

    Fix test specs project

--- test/specs/ClientSteps.cs
+++ test/specs/ClientSteps.cs
@@ -1,74 +1,74 @@
 ﻿using TechTalk.SpecFlow;
 
-namespace TDL.Test
+namespace TDL.Test.Specs
 {
-	[Binding]
-	public class ClientSteps
-	{
-		[Given( @"I start with a clean broker" )]
-		public void GivenIStartWithACleanBroker()
-		{
-			ScenarioContext.Current.Pending();
-		}
+    [Binding]
+    public class ClientSteps
+    {
+        [Given(@"I start with a clean broker")]
+        public void GivenIStartWithACleanBroker()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
-		[Given( @"I receive the following requests:" )]
-		public void GivenIReceiveTheFollowingRequests( Table table )
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Given( @"the broker is not available" )]
-		public void GivenTheBrokerIsNotAvailable()
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[When( @"I go live with the following processing rules:" )]
-		public void WhenIGoLiveWithTheFollowingProcessingRules( Table table )
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should consume all requests" )]
-		public void ThenTheClientShouldConsumeAllRequests()
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should publish the following responses:" )]
-		public void ThenTheClientShouldPublishTheFollowingResponses( Table table )
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should not consume any request" )]
-		public void ThenTheClientShouldNotConsumeAnyRequest()
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should not publish any response" )]
-		public void ThenTheClientShouldNotPublishAnyResponse()
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should consume first request" )]
-		public void ThenTheClientShouldConsumeFirstRequest()
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"the client should display to console:" )]
-		public void ThenTheClientShouldDisplayToConsole( Table table )
-		{
-			ScenarioContext.Current.Pending();
-		}
-
-		[Then( @"I should get no exception" )]
-		public void ThenIShouldGetNoException()
-		{
-			ScenarioContext.Current.Pending();
-		}
-	}
+        [Given(@"I receive the following requests:")]
+        public void GivenIReceiveTheFollowingRequests(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the broker is not available")]
+        public void GivenTheBrokerIsNotAvailable()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I go live with the following processing rules:")]
+        public void WhenIGoLiveWithTheFollowingProcessingRules(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should consume all requests")]
+        public void ThenTheClientShouldConsumeAllRequests()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should publish the following responses:")]
+        public void ThenTheClientShouldPublishTheFollowingResponses(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should not consume any request")]
+        public void ThenTheClientShouldNotConsumeAnyRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should not publish any response")]
+        public void ThenTheClientShouldNotPublishAnyResponse()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should consume first request")]
+        public void ThenTheClientShouldConsumeFirstRequest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the client should display to console:")]
+        public void ThenTheClientShouldDisplayToConsole(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should get no exception")]
+        public void ThenIShouldGetNoException()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
 }
--- test/specs/Properties/AssemblyInfo.cs
+++ test/specs/Properties/AssemblyInfo.cs
@@ -2,17 +2,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle( "TDL.Test.Specs" )]
-[assembly: AssemblyDescription( "" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyProduct( "TDL.Test.Specs" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyTitle("Test.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible( false )]
+[assembly: ComVisible(false)]
 
-[assembly: Guid( "d51e6f34-4cf7-4bbe-abc0-720cf92f937b" )]
+[assembly: Guid("31278a78-1321-40be-8c99-02ed9fd9e45e")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
