b312245a04321040f0299539ed0a11c5f8b050c9
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-09-19 04:18:15 +0300

    Remove jolokia requests from client

--- src/client/TdlClient.cs
+++ src/client/TdlClient.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using RestSharp;
 
 namespace TDL.Client
 {
@@ -18,28 +16,6 @@ namespace TDL.Client
             Port = port;
             UniqueId = uniqueId;
             TimeToWaitForRequests = timeToWaitForRequests;
-        }
-
-        public void Request(Dictionary<string, object> jolokiaPayload)
-        {
-            var client = new RestClient($"http://{Host}:{Port}/api/jolokia");
-
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "application/json");
-            request.AddJsonBody(jolokiaPayload);
-
-            var response = client.Execute(request);
-        }
-
-        public void SendTextMessage(string message)
-        {
-            Request(new Dictionary<string, object>
-            {
-                ["type"] = "exec",
-                ["mbean"] = "org.apache.activemq:type=Broker,brokerName=TEST.BROKER,destinationType=Queue,destinationName=TEST.QUEUE",
-                ["operation"] = "sendTextMessage(java.lang.String)",
-                ["arguments"] = new List<string> {message}
-            });
         }
     }
 }
--- src/demo/Program.cs
+++ src/demo/Program.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using TDL.Client;
-
-namespace TDL.Demo
+﻿namespace TDL.Demo
 {
     internal class Program
     {
         private static void Main()
         {
-            var tdlClient = new TdlClient(
-                "localhost",
-                28161,
-                "testuser@example.com",
-                TimeSpan.Zero);
-
-            tdlClient.SendTextMessage("test");
         }
     }
 }
