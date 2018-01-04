cb0f1ffc022112de1ad5ed8f2163af04cb6f8b42
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2018-01-04 11:34:09 +0800

    Trying to fix hanging

--- run-tests.cmd
+++ run-tests.cmd
@@ -1,17 +1,9 @@
-echo Starting WireMock process #1
-python wiremock/fetch-wiremock-and-run.py start 41375
-echo Started WireMock process #1
-
-echo Starting WireMock process #2
-python wiremock/fetch-wiremock-and-run.py start 8222
-echo Started WireMock process #2
-
-echo Starting broker process
+python fetch-wiremock-and-run.py start 41375
+python fetch-wiremock-and-run.py start 8222
 python broker/activemq-wrapper.py start
-echo Started broker process
 
 nunit3-console test/specs/bin/Release/TDL.Test.Specs.dll
 
-python wiremock/fetch-wiremock-and-run.py stop 41375
-python wiremock/fetch-wiremock-and-run.py stop 8222
+python fetch-wiremock-and-run.py stop 41375
+python fetch-wiremock-and-run.py stop 8222
 python broker/activemq-wrapper.py stop
