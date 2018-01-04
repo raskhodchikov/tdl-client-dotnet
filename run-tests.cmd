b6e3a93ddf0ca479b14eb2e315fbd325a4d87db0
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2018-01-04 12:34:57 +0800

    Update run-tests.cmd

--- run-tests.cmd
+++ run-tests.cmd
@@ -1,9 +1,10 @@
 python fetch-wiremock-and-run.py start 41375
 python fetch-wiremock-and-run.py start 8222
-:: python broker/activemq-wrapper.py start
+python broker/activemq-wrapper.py start
 
 nunit3-console test/specs/bin/Release/TDL.Test.Specs.dll
+EXIT %errorlevel%
+::python fetch-wiremock-and-run.py stop 41375
+::python fetch-wiremock-and-run.py stop 8222
 
-python fetch-wiremock-and-run.py stop 41375
-python fetch-wiremock-and-run.py stop 8222
 :: python broker/activemq-wrapper.py stop
